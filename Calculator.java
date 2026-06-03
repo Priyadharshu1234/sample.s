4794bbe6a8b876ad78aa784eb8c0c51d4c31e9db
Priyadharshu1234 <11f23.priyadharshiny.r@gmail.com>
2026-06-03 16:14:07 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,14 @@
+class calculator{
+    public int add(int a, int b){
+        return a+b;
+        }
+        public int add(int a, int b,int c){
+            return a+b+c;
+        }
+}
+public class PolymorphismEx{
+    public static void main(Strings []args){
+        Calculator calc=new Calculator();
+        System.out.println(calc.add(a:11,b:22:c:33));
+    }
+}
